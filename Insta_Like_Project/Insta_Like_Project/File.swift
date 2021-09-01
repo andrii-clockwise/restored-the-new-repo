89bc6943185eaa40dc543d20940fbe6c3b00c002
andrii-clockwise <51157903+andrii-clockwise@users.noreply.github.com>
2021-09-01 17:24:16 +0300

    Add files via upload

---
+++ Insta_Like_Project/Insta_Like_Project/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Insta_Like_Project
+//
+//  Created by Andrew Mokin on 15.12.2019.
+//  Copyright © 2019 Andrew Mokin. All rights reserved.
+//
+
+import Foundation
---
+++ Insta_Like_Project/Insta_Like_Project/HomeScreen.swift
@@ -0,0 +1,20 @@
+//
+//  HomeScreen.swift
+//  Insta_Like_Project
+//
+//  Created by Andrew Mokin on 15.12.2019.
+//  Copyright © 2019 Andrew Mokin. All rights reserved.
+//
+
+import UIKit
+
+class HomeScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
